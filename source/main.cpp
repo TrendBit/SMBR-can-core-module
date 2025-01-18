f3e1826ba3e6ae4097c18c91ef886c9f64329395
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-01-18 21:25:07 +0100

    main: move testing functionality into core_module

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,14 @@
 #include "main.hpp"
+#include "core_module.hpp"
 
 int main() {
     try {
-        // Open the I2C bus
         I2C_bus i2c("/dev/i2c-1");
-
-        auto interface_module = new Interface_module(&i2c);
-
-        std::cout << "5V sense: " << std::fixed << std::setprecision(3) << interface_module->Voltage_5V() << " V" << std::endl;
-        std::cout << "Vin sense: " << std::fixed << std::setprecision(3) << interface_module->Voltage_Vin() << " V" << std::endl;
-        std::cout << "PoE sense: " << std::fixed << std::setprecision(3) << interface_module->Voltage_PoE() << " V" << std::endl;
-        std::cout << "Current sense: " << std::fixed << std::setprecision(3) << interface_module->Current() << " A" << std::endl;
-        std::cout << "Power draw: " << std::fixed << std::setprecision(3) << interface_module->Power_draw() << " W" << std::endl;
-        bool poe_status = interface_module->PoE_status();
-        std::cout << "PoE status: " << (poe_status ? "Connected" : "Disconnected") << std::endl;
-        if (poe_status) {
-            std::cout << "PoE budget: " << (interface_module->PoE_budget() ? "Basic" : "Plus") << std::endl;
-        }
-        std::cout << "Vin status: " << (interface_module->Vin_status() ? "Connected" : "Disconnected") << std::endl;
-
+        Core_module core(&i2c);
+        core.Test();
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return -1;
     }
-
     return 0;
 }
--- source/main.hpp
+++ source/main.hpp
@@ -2,13 +2,14 @@
  * @file main.hpp
  * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
  * @version 0.1
- * @date 06.12.2024
+ * @date 18.01.2025
  */
 
-#include "i2c_bus.hpp"
-#include "i2c_device.hpp"
-#include "TLA2024.hpp"
-#include "interface_module.hpp"
+#pragma once
+
+#include "core_module.hpp"
+#include "hw/i2c_bus.hpp"
+#include "hw/i2c_device.hpp"
 
 #include <iostream>
 #include <iomanip>
