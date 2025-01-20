cfa143e1578b1a61253e36b19e1e991b7ae06011
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-01-20 15:34:56 +0100

    interface_board: reduce conversion delay of adc measurement

--- source/interface_board.cpp
+++ source/interface_board.cpp
@@ -81,7 +81,7 @@ float Interface_board::Board_temperature()
 float Interface_board::Measure_voltage(TLA2024::Channels channel)
 {
     adc->Select_channel(channel);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
     float voltage = adc->Read_voltage().value_or(0.0f);
     return voltage;
 }
